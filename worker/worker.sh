32d3209f5c898d02f868ec728b3b32e2a0bef9de
Alexander Martin <accesstoalex@gmail.com>
2021-06-16 13:30:17 +0200

    add host option to worker

--- worker/worker.sh
+++ worker/worker.sh
@@ -11,4 +11,4 @@ mkdir -p $SPARK_WORKER_LOG
 ln -sf /dev/stdout $SPARK_WORKER_LOG/spark-worker.out
 
 /spark/sbin/../bin/spark-class org.apache.spark.deploy.worker.Worker \
-    --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG/spark-worker.out
+    --host $SPARK_WORKER_HOST  --webui-port $SPARK_WORKER_WEBUI_PORT $SPARK_MASTER >> $SPARK_WORKER_LOG/spark-worker.out
